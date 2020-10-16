715e2325d3d813262ca6185756cac24b8c0e60e3
74kw <st55427@student.upce.cz>
2020-10-16 14:59:03 +0200

    Test funkčnosti

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
+?>
+<html lang="cs">
+    <?php include 'layout/header.phtml'?>
+    <body>
+        <main>
+            <!--     <?php //include 'layout/navigation.phtml'?>
+            <section>
+                <?php
+                #if($_SERVER['REQUEST_URI'] !== "/")
+                #    require_once ("./pages" . strtok($_SERVER['REQUEST_URI'], '?') . ".phtml");
+                #else
+                #    require_once ("./pages/index.phtml");
+                #?>
+            </section>-->
+        </main>
+            <?php include 'layout/footer.phtml'; ?>
+          
+    </body>
+</html>
+
+
+
+
 
